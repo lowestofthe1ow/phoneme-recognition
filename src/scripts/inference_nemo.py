352d4035dc4d9fbe4e5e0c1b7ecca2c413823c90
Lorenz Bernard Marqueses <lorenz_marqueses@dlsu.edu.ph>
2026-01-31 18:46:20 +0800

    Checkout src/scripts/inference_nemo.py from branch tokenizer/lingua

---
+++ src/scripts/inference_nemo.py
@@ -0,0 +1,19 @@
+import argparse
+import nemo.collections.asr as nemo_asr
+
+from torchinfo import summary
+
+parser = argparse.ArgumentParser(
+    description="Inspects a .nemo model at a given path."
+)
+parser.add_argument("path", help="The input .nemo model file path")
+args = parser.parse_args()
+
+model = nemo_asr.models.ASRModel.restore_from(args.path)
+
+# Transcribe the converted WAV file
+# The input is a list of file paths
+transcriptions = model.transcribe("data/samples/TAGFND1_0898.wav")
+
+# Print the transcription
+print(transcriptions[0].text)
